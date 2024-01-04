510a59191f6690ef4c83945467790702affec406
-_- <atsgula@gmail.com>
2024-01-03 22:25:56 -0500

    remove download btn

--- resources/views/pages/sign-in-static.blade.php
+++ resources/views/pages/sign-in-static.blade.php
@@ -48,12 +48,12 @@
                                     </a>
                                 </li>
                             </ul>
-                            <ul class="navbar-nav d-lg-block d-none">
+                            {{-- <ul class="navbar-nav d-lg-block d-none">
                                 <li class="nav-item">
                                     <a href="https://www.creative-tim.com/product/argon-dashboard-laravel" target="_blank"
                                         class="btn btn-sm mb-0 me-1 btn-primary">Free Download</a>
                                 </li>
-                            </ul>
+                            </ul> --}}
                         </div>
                     </div>
                 </nav>
