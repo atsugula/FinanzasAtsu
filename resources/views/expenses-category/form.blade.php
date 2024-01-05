3012283dc25e9979c59c19a5a433042a72133a6d
-_- <atsgula@gmail.com>
2024-01-05 17:37:59 -0500

    add module Expenses Category

---
+++ resources/views/expenses-category/form.blade.php
@@ -0,0 +1,15 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('name') }}
+            {{ Form::text('name', $expensesCategory->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
+            {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+
+    {{-- Boton para todo --}}
+    @include('layouts.btn-submit')
+    
+</div>
