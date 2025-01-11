1f6698d22a37954fdbd09d886fd44cddbf6d2d90
-_- <111021816+atsugula@users.noreply.github.com>
2025-01-11 13:12:29 -0500

    add default columns of type string

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // default columns string
+        Schema::defaultStringLength(191);
     }
 }
