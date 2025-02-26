b9458eb90454947a9eddb7bc8e25146f8fc21c83
-_- <111021816+atsugula@users.noreply.github.com>
2025-02-25 20:07:05 -0500

    add register

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\TransactionController;
 
@@ -15,9 +16,10 @@ use App\Http\Controllers\Api\V1\TransactionController;
 |--------------------------------------------------------------------------
 */
 
-Route::prefix('v1/auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::middleware('api.auth')->post('/logout', [AuthController::class, 'logout']);
+Route::prefix('v1')->group(function () {
+    Route::post('/auth/login', [AuthController::class, 'login']);
+    Route::middleware('api.auth')->post('/auth/logout', [AuthController::class, 'logout']);
+    Route::post('/register', [RegisterController::class, 'store'])->middleware('guest')->name('register.perform');
 });
 
 Route::middleware('api.auth')->prefix('v1')->group(function () {
