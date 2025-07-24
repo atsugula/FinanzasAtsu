674e940a1f3bc86160da7e36ffe7f8739bd696f5
-_- <111021816+atsugula@users.noreply.github.com>
2025-07-24 13:47:50 -0500

    fix styles panel

--- resources/views/components/message-dashboard.blade.php
+++ resources/views/components/message-dashboard.blade.php
@@ -1,12 +1,17 @@
 <div
-    class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-    <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
+    class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column overflow-hidden">
+    <div class="position-relative bg-gradient-primary h-100 w-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center"
         style="background-image: url('{{ asset('img/home.png') }}');
-                                        background-size: cover;">
+               background-size: cover;
+               background-position: center;
+               min-height: 0; /* Permite que el contenedor se ajuste */">
         <span class="mask bg-gradient-primary opacity-6"></span>
-        <h4 class="mt-5 text-white font-weight-bolder position-relative">
-            {{ __('"Turn every expense into a smart decision."') }}</h4>
-        <p class="text-white position-relative">
-            {{ __("It's not about how much you earn, but how you manage it. Purposeful finance, starting today.") }}</p>
+        <div class="position-relative z-index-1 py-4">
+            <h4 class="text-white font-weight-bolder">
+                {{ __('"Turn every expense into a smart decision."') }}</h4>
+            <p class="text-white mb-0">
+                {{ __("It's not about how much you earn, but how you manage it. Purposeful finance, starting today.") }}
+            </p>
+        </div>
     </div>
 </div>
